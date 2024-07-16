be0c71e35e4b027428257a21756f44172112c3d6
Neha-0713 <nrai52703@gmail.com>
2024-07-16 13:24:34 +0530

    signup page ui

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
-const Signin  = () => {
+const Signup = () => {
   return (
-    <div> 
-        <div className='w-30% flex justify-center'>
-        <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700">
+    <div>
+      <div className='flex justify-center w-30% mb-10'>
+      <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700">
     <div className="p-4 sm:p-7">
       <div className="text-center">
         <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
-          Sign in
+          Sign up
         </h1>
         <p className="mt-2 text-sm text-gray-600 dark:text-neutral-400">
-          Don't have an account yet?
+          Already have an account?
           <a
             className="text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500"
-            href="../examples/html/signup.html"
+            href="../examples/html/signin.html"
           >
-            Sign up here
+            Sign in here
           </a>
         </p>
       </div>
@@ -49,7 +49,7 @@ const Signin  = () => {
               fill="#EB4335"
             />
           </svg>
-          Sign in with Google
+          Sign up with Google
         </button>
         <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-600 dark:after:border-neutral-600">
           Or
@@ -94,20 +94,12 @@ const Signin  = () => {
             {/* End Form Group */}
             {/* Form Group */}
             <div>
-              <div className="flex justify-between items-center">
-                <label
-                  htmlFor="password"
-                  className="block text-sm mb-2 dark:text-white"
-                >
-                  Password
-                </label>
-                <a
-                  className="text-sm text-blue-600 decoration-2 hover:underline font-medium"
-                  href="../examples/html/recover-account.html"
-                >
-                  Forgot password?
-                </a>
-              </div>
+              <label
+                htmlFor="password"
+                className="block text-sm mb-2 dark:text-white"
+              >
+                Password
+              </label>
               <div className="relative">
                 <input
                   type="password"
@@ -135,6 +127,44 @@ const Signin  = () => {
               </p>
             </div>
             {/* End Form Group */}
+            {/* Form Group */}
+            <div>
+              <label
+                htmlFor="confirm-password"
+                className="block text-sm mb-2 dark:text-white"
+              >
+                Confirm Password
+              </label>
+              <div className="relative">
+                <input
+                  type="password"
+                  id="confirm-password"
+                  name="confirm-password"
+                  className="py-3 px-4 block w-full border-gray-200 border-2 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                  required=""
+                  aria-describedby="confirm-password-error"
+                />
+                <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                  <svg
+                    className="size-5 text-red-500"
+                    width={16}
+                    height={16}
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                    aria-hidden="true"
+                  >
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                  </svg>
+                </div>
+              </div>
+              <p
+                className="hidden text-xs text-red-600 mt-2"
+                id="confirm-password-error"
+              >
+                Password does not match the password
+              </p>
+            </div>
+            {/* End Form Group */}
             {/* Checkbox */}
             <div className="flex items-center">
               <div className="flex">
@@ -147,7 +177,13 @@ const Signin  = () => {
               </div>
               <div className="ms-3">
                 <label htmlFor="remember-me" className="text-sm dark:text-white">
-                  Remember me
+                  I accept the{" "}
+                  <a
+                    className="text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500"
+                    href="#"
+                  >
+                    Terms and Conditions
+                  </a>
                 </label>
               </div>
             </div>
@@ -156,7 +192,7 @@ const Signin  = () => {
               type="submit"
               className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
             >
-              Sign in
+              Sign up
             </button>
           </div>
         </form>
@@ -169,4 +205,4 @@ const Signin  = () => {
   )
 }
 
-export default Signin 
+export default Signup
