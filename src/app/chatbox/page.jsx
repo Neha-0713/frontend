5622aacf20789a094f84f2e594ec968f18089ee4
Neha-0713 <nrai52703@gmail.com>
2024-07-20 23:51:57 +0530

    chatbox ui

--- src/app/chatbox/page.jsx
+++ src/app/chatbox/page.jsx
@@ -1,33 +1,16 @@
-'use client';
-import React, { useState } from 'react'
-
+import React from 'react'
 
 const chatbox = () => {
-  const [message,setMessage]=useState([]);
-
-    
-    const sendMessage=(e)=>{
-        console.log(e.target.value);
-        const newMsg={text:e.target.value};
-        
-        setMessage([...message,newMsg])
-        e.target.value='';
-
-      
-        }
-
   return (
-    <div className='bg-gray-200 fixed bottom-0 w-full py-10 max-w-[80%] mx-auto shadow-lg '>
-        <div className='flex'>
-        <input className='w-full bg-gray-100 rounded-sm' type="text" />
-        <button className='w-auto  bg-gray-500 text-white rounded-lg px-3 py-2' onClick={sendMessage}>send</button>
+    <div>
+      <h1 className='font-bold font-3xl text-center'>Chat Box</h1>
+      <div className='flex flex-row justify-center w-full mx-auto fixed bottom-0 my-3'>
+        
+        <input className='border-gray-300 rounded-md p-2 w-1/4' type="text" />
+        <button className='bg-gray-500 rounded-lg px-3 py-1 absolute ml-80 mt-1 mr-2 text-white'>Send</button>
         </div>
-        <div>{
-          message.map((a)=>{return a.message})
-          }
-          
-        </div>
-    </div>
+      </div>
+    
   )
 }
 
